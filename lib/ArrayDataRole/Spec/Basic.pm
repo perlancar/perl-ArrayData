dc58b7f02ef8cff9ccd5108ceb1410b4df142b05
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-01-16 10:30:50 +0700

    [spec Basic] Require 'get_item_count'; [doc] Document provided methods from Role::TinyCommons::Iterator::Resettable and show a table of methods like in TableData

--- lib/ArrayDataRole/Spec/Basic.pm
+++ lib/ArrayDataRole/Spec/Basic.pm
@@ -14,6 +14,9 @@ use Role::Tiny::With;
 # constructor
 requires 'new';
 
+# other required methods
+requires 'get_item_count';
+
 # mixin
 with 'Role::TinyCommons::Iterator::Resettable';
 with 'Role::TinyCommons::Collection::GetItemByPos';
@@ -61,6 +64,24 @@ interface (L<Role::TinyCommons::Iterator::Resettable>) as well as get elements
 by position (L<Role::TinyCommons::Collection::GetItemByPos>), like what a
 regular Perl array lets you.
 
+It allows an array with infinite number of elements.
+
+ category                     method name                note                        modifies iterator?
+ --------                     -----------                -------                     ------------------
+ instantiating                new(%args)                                             N/A
+
+ iterating items              has_next_item()                                        no
+                              get_next_item()                                        yes (moves forward 1 position)
+                              reset_iterator()                                       yes (resets)
+
+ iterating items (alt)        each_item()                                            yes (resets)
+
+ getting all items            get_all_items()                                        yes (resets)
+
+ getting item count           get_item_count()                                       yes (resets) / no (for some implementations)
+
+ utility: applying roles      apply_roles(R1, R2, ...)                               N/A
+
 
 =head1 ROLES MIXED IN
 
@@ -95,12 +116,36 @@ From L<Role::TinyCommons::Iterator::Resettable>.
 
 From L<Role::TinyCommons::Collection::GetItemByPos>.
 
+Should reset iterator.
+
 =head2 has_item_at_pos
 
 From L<Role::TinyCommons::Collection::GetItemByPos>.
 
+Can reset iterator if required.
+
+=head2 get_item_count
+
+Provided by L<Role::TinyCommons::Collection::GetItemByPos>, but you can provide
+a more efficient implementation.
+
+Get number of elements of array. Must return -1 to mean an array with infinite
+number of elements. Can reset iterator if required.
+
 
 =head1 PROVIDED METHODS
+
+=head2 each_item
+
+From L<Role::TinyCommons::Collection::GetItemByPos>. But if you have an infinite
+array data, this method will loop endlessly; you can provide your own
+implementation to prevent this from happening.
+
+=head2 get_all_items
+
+From L<Role::TinyCommons::Collection::GetItemByPos>. But if you have an infinite
+array data, this method will loop and collect endlessly; you can provide your
+own implementation to prevent this from happening.
 
 =head2 apply_roles
 
@@ -115,7 +160,7 @@ namespace. It's a convenience shortcut for C<< Role::Tiny->apply_roles_to_object
 
 Return the object, so you can do something like this:
 
- my $obj = ArrayData::Word::ID::KBBI->new->apply_roles('FindItem::Iterator', 'PickItems::Iterator');
+ my $obj = ArrayData::Word::ID::KBBI->new->apply_roles('FindItem::Iterator', 'PickItems::RandomPos');
 
  my $obj = ArrayData::Word::ID::KBBI->new->apply_roles('BinarySearch::LinesInHandle');
 
